55930efe380025eae41e8f402a9fcf11d6b89536
Roby Sunjaya <robysunjaya20@gmail.com>
2026-09-06 18:29:59 +0700

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,13 +14,44 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Quality Training",
-  description: "Quality, Automotive Core Tools and Continuous Improvement Training Platform",
+  description:
+    "Quality, Automotive Core Tools and Continuous Improvement Training Platform",
+
+  openGraph: {
+    title: "Quality Training",
+    description:
+      "Quality, Automotive Core Tools and Continuous Improvement Training Platform",
+    url: "https://bei-quality-training.vercel.app/",
+    siteName: "Quality Training",
+    images: [
+      {
+        url: "/og_bei.png",
+        width: 800,
+        height: 450,
+        alt: "PT Bumjin Electronics Indonesia - Quality Training",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Quality Training",
+    description:
+      "Quality, Automotive Core Tools and Continuous Improvement Training Platform",
+    images: ["/og_bei.png"],
+  },
+
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="id"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
